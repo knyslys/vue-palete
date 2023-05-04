b2fd91d763dd4e9d6ef8fcfacab293d4adbc58d3
knyslys <vakaris.serbenta7@gmail.com>
2023-05-04 14:21:31 +0300

    .

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <!-- <RouterView /> -->
+  <div
+    class="container mx-auto px-5 text-center bg-[#50d71e] hover:bg-[#000] hover:text-[#fff] transition-all duration-1000"
+  >
+    Hello
+  </div>
+</template>
+
+<style scoped></style>
